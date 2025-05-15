66472446f9f3553f664d93e82597cd0f9ca758c9
Tran Hoang Giang <Hgiangtb2793@gmail.com>
2025-05-15 20:41:33 +0700

    Time: 0 ms (100.00%) | Memory: 51.2 MB (24.63%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cs
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cs
@@ -1,11 +1,26 @@
 public class Solution {
+    // No need to track the last unique index of the current value. Only need to know when the value change since:
+    // 1. It is sorted, so the changes mean that the i-pointer have reached the next desirable value
+    // 2. We already know the index of where the value need to be inserted with k
+    // => We know both the index and value.
+
+    // public int RemoveDuplicates(int[] nums) {
+    //     int k = 1;
+    //     int lastUniqueIdx = 0;
+    //     for(int i = 1; i < nums.Length; i++){
+    //         if(nums[i] != nums[lastUniqueIdx]){
+    //             nums[k++] = nums[i];
+    //             lastUniqueIdx = i;
+    //         }
+    //     }
+    //     return k;
+    // }
+
     public int RemoveDuplicates(int[] nums) {
         int k = 1;
-        int lastUniqueIdx = 0;
         for(int i = 1; i < nums.Length; i++){
-            if(nums[i] != nums[lastUniqueIdx]){
+            if(nums[i] != nums[i - 1]){
                 nums[k++] = nums[i];
-                lastUniqueIdx = i;
             }
         }
         return k;
