14ad3fed9a1331943bd1febe4cd341cfa6472ae3
Tran Hoang Giang <Hgiangtb2793@gmail.com>
2025-05-15 20:36:16 +0700

    Time: 0 ms (100.00%) | Memory: 51.2 MB (35.79%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        int k = 1;
+        int lastUniqueIdx = 0;
+        for(int i = 1; i < nums.Length; i++){
+            if(nums[i] != nums[lastUniqueIdx]){
+                nums[k++] = nums[i];
+                lastUniqueIdx = i;
+            }
+        }
+        return k;
+    }
+}
